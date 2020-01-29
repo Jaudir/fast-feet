6b7327078d85d8bb35b020ada20cff06ace1e806
jaudir <5773247+jaudir@users.noreply.github.com>
2020-01-29 19:56:31 -0300

    feat: Recipient controller without validation

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,36 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({});
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({});
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    const recipient_update = await recipient.update(req.body);
+
+    return res.json(recipient_update);
+  }
+  /*
+  index() {}
+  destroy() {}
+  show() {} */
+}
+
+export default new RecipientController();
