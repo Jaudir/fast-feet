d02a63203f3e669eafc0f736a482599ba2fdaa81
jaudir <5773247+jaudir@users.noreply.github.com>
2020-01-29 19:57:21 -0300

    feat: create routes users and sessions

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,25 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // ROUTES
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+// USER
+
+// RECIPENT
+
+// SESSION
 
 export default routes;
