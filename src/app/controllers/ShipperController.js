9d2c49f858f33ca727aa64d656a4b230f9bdb8dd
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-05 13:38:40 -0300

    feat: Add new shippers routes

---
+++ src/app/controllers/ShipperController.js
@@ -0,0 +1,19 @@
+class ShipperController {
+  async store(req, res) {
+    return res.send();
+  }
+
+  async index(req, res) {
+    return res.send();
+  }
+
+  async destroy(req, res) {
+    return res.send();
+  }
+
+  async update(req, res) {
+    return res.send();
+  }
+}
+
+export default new ShipperController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import ShipperController from './app/controllers/ShipperController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,10 +14,17 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
+// Required to logon
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.post('/shippers', ShipperController.store);
+routes.get('/shippers', ShipperController.index);
+routes.delete('/shippers/:shipperId', ShipperController.destroy);
+routes.put('/shippers/:shipperId', ShipperController.update);
 
 export default routes;
