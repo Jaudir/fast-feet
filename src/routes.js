48d93688fa61df9573e1b56e61a7944ef641369c
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-05 14:07:56 -0300

    feat: Creat File functionality

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import ShipperController from './app/controllers/ShipperController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // ROUTES
 
@@ -26,5 +30,7 @@ routes.post('/shippers', ShipperController.store);
 routes.get('/shippers', ShipperController.index);
 routes.delete('/shippers/:shipperId', ShipperController.destroy);
 routes.put('/shippers/:shipperId', ShipperController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
