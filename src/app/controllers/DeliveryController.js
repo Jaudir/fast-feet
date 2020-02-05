4de308ea2f028bc87c763675d37f5305678d116e
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-05 18:36:16 -0300

    feat: add store function

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,8 +1,21 @@
+import * as Yup from 'yup';
 import Delivery from '../models/Delivery';
 
 class DeliveryController {
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      product: Yup.string().required(),
+      recipient_id: Yup.number().required(),
+      shipper_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const delivery = await Delivery.create(req.body);
+
+    return res.json(delivery);
   }
 
   async index(req, res) {
