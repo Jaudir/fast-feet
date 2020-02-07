3292390d7d22b6f6f3a8840056d1bf44a5257d92
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-07 11:37:07 -0300

    feat: Add -> cancel delivery throuth problem id

--- src/app/controllers/DeliveryProblemsController.js
+++ src/app/controllers/DeliveryProblemsController.js
@@ -1,8 +1,11 @@
 import Delivery from '../models/Delivery';
 import DeliveryProblem from '../models/DeliveryProblem';
 
+import Queue from '../../lib/Queue';
+
 import Shipper from '../models/Shipper';
 import Recipient from '../models/Recipient';
+import CancellationDeliveryMail from '../jobs/CancellationDeliveryMail';
 
 class DeliveryProblemsController {
   async index(req, res) {
@@ -33,7 +36,45 @@ class DeliveryProblemsController {
   }
 
   async destroy(req, res) {
-    return res.json();
+    const { problemId } = req.params;
+
+    const problem = await DeliveryProblem.findByPk(problemId);
+
+    if (!problem) {
+      return res.status(401).json({ error: 'Problem  not found!' });
+    }
+    const deliveryId = problem.delivery_id;
+    const delivery = await Delivery.findByPk(deliveryId, {
+      include: [
+        {
+          model: Shipper,
+          as: 'shipper',
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['name', 'number', 'zipcode', 'street', 'city', 'state'],
+        },
+      ],
+    });
+
+    if (!delivery) {
+      return res.status(401).json({ error: 'Delivery not found!' });
+    }
+
+    if (delivery.canceled_at !== null) {
+      return res.status(401).json({ error: 'Delivery already canceled' });
+    }
+    delivery.canceled_at = new Date();
+
+    await delivery.save();
+
+    await Queue.add(CancellationDeliveryMail.key, {
+      delivery,
+    });
+
+    return res.json({ problem, delivery });
   }
 }
 
