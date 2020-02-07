a66a4e11cfddc1a5885062632fc682259b98da4f
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-07 11:12:48 -0300

    feat: create func to list deliveries with problems

--- src/app/controllers/DeliveryProblemsController.js
+++ src/app/controllers/DeliveryProblemsController.js
@@ -1,9 +1,35 @@
 import Delivery from '../models/Delivery';
 import DeliveryProblem from '../models/DeliveryProblem';
 
+import Shipper from '../models/Shipper';
+import Recipient from '../models/Recipient';
+
 class DeliveryProblemsController {
   async index(req, res) {
-    return res.json();
+    const { page = 1 } = req.query;
+    const problems = await DeliveryProblem.findAll({
+      include: [
+        {
+          model: Delivery,
+          as: 'delivery',
+          include: [
+            {
+              model: Shipper,
+              as: 'shipper',
+              attributes: ['name'],
+            },
+            {
+              model: Recipient,
+              as: 'recipient',
+              attributes: ['name'],
+            },
+          ],
+        },
+      ],
+      offset: (page - 1) * 20,
+    });
+
+    return res.json(problems);
   }
 
   async destroy(req, res) {
