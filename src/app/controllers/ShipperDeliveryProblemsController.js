a2d5855894b029d49ca0f973b5783f224ad2afe2
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-07 10:59:59 -0300

    feat: Implement store func

--- src/app/controllers/ShipperDeliveryProblemsController.js
+++ src/app/controllers/ShipperDeliveryProblemsController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Delivery from '../models/Delivery';
 import DeliveryProblem from '../models/DeliveryProblem';
 
@@ -7,7 +8,28 @@ class ShipperDeliveryProblemsController {
   }
 
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      description: Yup.string().required(),
+    });
+    const { deliveryId } = req.params;
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const delivery = await Delivery.findByPk(deliveryId);
+
+    if (!delivery) {
+      return res.status(401).json({ error: 'Delivery do not exists.' });
+    }
+
+    const bodyProblem = {
+      description: req.body.description,
+      delivery_id: Number(deliveryId),
+    };
+    const problem = await DeliveryProblem.create(bodyProblem);
+
+    return res.json(problem);
   }
 }
 
