b0bfbc7a6b9c130f166e9b9e9d518481c23d5a76
jaudir <5773247+jaudir@users.noreply.github.com>
2020-02-07 09:44:15 -0300

    feat: create files about delivery problems

---
+++ src/app/controllers/DeliveryProblemsController.js
@@ -0,0 +1,14 @@
+import Delivery from '../models/Delivery';
+import DeliveryProblem from '../models/DeliveryProblem';
+
+class DeliveryProblemsController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async delete(req, res) {
+    return res.json();
+  }
+}
+
+export default new DeliveryProblemsController();
---
+++ src/app/controllers/ShipperDeliveryProblemsController.js
@@ -0,0 +1,14 @@
+import Delivery from '../models/Delivery';
+import DeliveryProblem from '../models/DeliveryProblem';
+
+class ShipperDeliveryProblemsController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new ShipperDeliveryProblemsController();
